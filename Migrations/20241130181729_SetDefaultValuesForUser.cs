004d6b07267ebf5063d901f12afb7726a27e7949
Hilori <hilori.furan@wp.pl>
2024-11-30 19:36:01 +0100

    migracja

---
+++ Migrations/20241130181729_SetDefaultValuesForUser.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NewsletterWebApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class SetDefaultValuesForUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Admin",
+                table: "Users",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Admin",
+                table: "Users");
+        }
+    }
+}

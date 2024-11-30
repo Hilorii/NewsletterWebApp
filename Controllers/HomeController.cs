84659371409a8f646c202c77518abf3f0ee232b6
Hilori <hilori.furan@wp.pl>
2024-11-30 08:53:19 +0100

    wświetlanie uzytkoników w home :D

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -2,21 +2,29 @@ using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using NewsletterWebApp.Models;
 using System.Data;
+using NewsletterWebApp.Data;
+using NewsletterWebApp.ViewModels;
 
 namespace NewsletterWebApp.Controllers;
 
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly DataContext context;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, DataContext context)
     {
         _logger = logger;
+        this.context = context;
     }
 
     public IActionResult Index()
     {
-        return View();
+        var users = this.context.Users.Select(u => new UserViewModel
+        {
+            Email = u.Email
+        });
+        return View(users);
     }
 
     public IActionResult Privacy()
---
+++ ViewModels/UserViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace NewsletterWebApp.ViewModels
+{
+    public class UserViewModel
+    {
+        public string Email { get; set; }
+    }
+}

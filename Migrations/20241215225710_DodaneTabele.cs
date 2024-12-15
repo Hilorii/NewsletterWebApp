393149789bc466a965106c077f9abe75111f5bf8
Hilori <hilori.furan@wp.pl>
2024-12-16 00:39:14 +0100

    .

---
+++ Migrations/20241215225710_DodaneTabele.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace NewsletterWebApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class DodaneTabele : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "TotalClicks",
+                table: "Emails",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "TotalOpens",
+                table: "Emails",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "EmailOpens",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn),
+                    EmailLogId = table.Column<int>(type: "integer", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EmailOpens", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_EmailOpens_EmailLogs_EmailLogId",
+                        column: x => x.EmailLogId,
+                        principalTable: "EmailLogs",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EmailOpens_EmailLogId",
+                table: "EmailOpens",
+                column: "EmailLogId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "EmailOpens");
+
+            migrationBuilder.DropColumn(
+                name: "TotalClicks",
+                table: "Emails");
+
+            migrationBuilder.DropColumn(
+                name: "TotalOpens",
+                table: "Emails");
+        }
+    }
+}

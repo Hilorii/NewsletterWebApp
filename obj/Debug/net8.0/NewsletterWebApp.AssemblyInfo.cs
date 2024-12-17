5d999e2755423eeecfc23780b56cf32dfb53995c
g-krzychu <krzysio.gawl@poczta.onet.pl>
2024-12-17 23:26:57 +0100

    uspójnienie strony tworzenia newslettera ze stroną wysyłania maila

--- obj/Debug/net8.0/NewsletterWebApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/NewsletterWebApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NewsletterWebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3459f0fb963ef77f1d961dec172fffb655467f1a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b497d5014fa7f85ff579ca8e0eb64ba961cb5787")]
 [assembly: System.Reflection.AssemblyProductAttribute("NewsletterWebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NewsletterWebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

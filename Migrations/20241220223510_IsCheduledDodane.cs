a3ca363d5afe37c436e9f2490ed7f8d54f46f223
Hilori <hilori.furan@wp.pl>
2024-12-20 23:35:38 +0100

    .

---
+++ Migrations/20241220223510_IsCheduledDodane.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NewsletterWebApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class IsCheduledDodane : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsScheduled",
+                table: "Emails",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsScheduled",
+                table: "Emails");
+        }
+    }
+}

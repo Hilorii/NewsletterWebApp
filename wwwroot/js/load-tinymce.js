98955cc441ab74557c370fe56eb8cdf6ffa4300a
g-krzychu <krzysio.gawl@poczta.onet.pl>
2024-12-23 23:39:02 +0100

    przesunięcie kodu ładującego tinymce do oddzielnego pliku

---
+++ wwwroot/js/load-tinymce.js
@@ -0,0 +1,7 @@
+﻿tinymce.init({
+    selector: 'textarea',
+    menubar: false,
+    plugins: 'lists link image media code',
+    file_picker_types: 'file image media',
+    toolbar: 'undo redo | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image media | code',
+});
